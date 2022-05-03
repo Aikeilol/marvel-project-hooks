5e8fd0fddb6b9a17d1e88b629b7f14c35003afdb
Vadim Shabinevich <shabinevich.v@mxm.email>
2022-05-03 21:25:39 +0300

    add react-router 6.3.0

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
 import AppHeader from "../appHeader/AppHeader";
@@ -8,23 +8,19 @@ import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
 
-    return (
-        <Router>
-            <div className="app">
-                <AppHeader />
-                <main>
-                    <Switch>
-                        <Route exact path="/">
-                            <MainPage />
-                        </Route>
-                        <Route exact path="/comics">
-                            <ComicsPage />
-                        </Route>
-                    </Switch>
-                </main>
-            </div>
-        </Router>
-    )
+  return (
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/comics" element={<ComicsPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  )
 
 
 }
