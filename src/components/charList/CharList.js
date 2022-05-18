4c659c072605f70589c9c27e6e9e27476c67988f
Vadim Shabinevich <shabinevich.v@mxm.email>
2022-05-18 16:26:29 +0300

    add react-transition

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
+import { TransitionGroup, Transition } from 'react-transition-group';
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import './charList.scss';
@@ -21,7 +22,7 @@ const CharList = (props) => {
         getAllCharacters(offset).then(onCharListLoaded)
     }
 
-  
+
 
     const onCharListLoaded = (newCharList) => {
         if (newCharList.length < 9) {
@@ -41,41 +42,68 @@ const CharList = (props) => {
         refArr.current[id].focus();
     }
 
+    let duration = 300;
+
+    const defaultStyle = {
+        transition: `opacity ${duration}ms ease-in-out`,
+        opacity: 0,
+    }
+
+    const transitionStyles = {
+        entering: { opacity: 0 },
+        entered: { opacity: 1 },
+        exiting: { opacity: 0 },
+        exited: { opacity: 0 },
+    };
+
 
     let item = null
-
+    
     if (loading && !newListLoading) {
         item = <Spinner />
     } else {
         item = chars.map((char, i) => {
+            if(duration === 1200){
+                duration = 300;
+            }
+            duration = duration + 100
             return (
-                < li key={char.id}
-                    tabIndex={0}
-                    ref={el => refArr.current[i] = el}
-                    className="char__item"
-                    onClick={() => {
-                        props.onCharSelected(char.id)
-                        focusOnItem(i)
-                    }}
-                    onKeyPress={(e) => {
-                        if (e.key === ' ' || e.key === "Enter") {
-                            props.onCharSelected(char.id);
-                            focusOnItem(i);
-                        }
-                    }}
-                >
-                    <img src={char.thumbnail} style={{ objectFit: char.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? 'unset' : 'cover' }} alt="abyss" />
-                    <div className="char__name">{char.name}</div>
-                </li >
+                <Transition in={!loading} timeout={duration}>
+                    {state => (
+                        <div style={{
+                            ...defaultStyle,
+                            ...transitionStyles[state]
+                        }}>
+                            < li key={char.id}
+                                tabIndex={0}
+                                ref={el => refArr.current[i] = el}
+                                className="char__item"
+                                onClick={() => {
+                                    props.onCharSelected(char.id)
+                                    focusOnItem(i)
+                                }}
+                                onKeyPress={(e) => {
+                                    if (e.key === ' ' || e.key === "Enter") {
+                                        props.onCharSelected(char.id);
+                                        focusOnItem(i);
+                                    }
+                                }}
+                            >
+                                <img src={char.thumbnail} style={{ objectFit: char.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg' ? 'unset' : 'cover' }} alt="abyss" />
+                                <div className="char__name">{char.name}</div>
+                            </li >
+                        </div>
+                    )}
+                </Transition>
             )
         })
     }
 
     return (
         <div className="char__list">
-            <ul className="char__grid" style={{ display: loading && !newListLoading ? 'flex' : 'grid', justifyContent: 'center' }}>
+            <TransitionGroup className="char__grid" style={{ display: loading && !newListLoading ? 'flex' : 'grid', justifyContent: 'center' }}>
                 {item}
-            </ul>
+            </TransitionGroup>
             {ended === false ? <button
                 className="button button__main button__long"
                 disabled={newListLoading}
